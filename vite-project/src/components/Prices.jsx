7733c38f4966213e7c8a8645f6307ffc2dc94b7e
NaokiTM <ntsome21@gmail.com>
2024-07-29 17:54:24 +0100

    uploaded files

---
+++ vite-project/src/components/Prices.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReturnPrices from './returnPrices';
+
+const Prices = () => {
+
+  return (
+    <>
+        <div>prices</div>
+        <ReturnPrices />
+    </>
+  )
+}
+
+export default Prices
